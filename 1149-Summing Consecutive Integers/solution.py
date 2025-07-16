3ab5e661b62b9fd12981d7a138ec20a6094dd67e
Abu Bakkar Siddique Sharif <abubakkarsiddiquesharif@gmail.com>
2025-07-16 22:56:16 +0600

    solved(1149): Summing COnsecutive Integers

---
+++ 1149-Summing Consecutive Integers/solution.py
@@ -0,0 +1,13 @@
+sample = list(map(int, input().split()))
+
+A = sample[0]
+N = sample[-1]
+
+total = [A]
+
+for i in range(1, N):
+    A += 1
+    total.append(A)
+
+result = sum(total)
+print(result)
