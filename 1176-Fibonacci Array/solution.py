f08499ced764d3049777fd99e7b1f1f49289a2ec
Abu Bakkar Siddique Sharif <abubakkarsiddiquesharif@gmail.com>
2025-07-26 23:03:30 +0600

    solved(1176): Fibonacci Array

---
+++ 1176-Fibonacci Array/solution.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for i in range(T):
+    n = int(input())
+    sequence = [0, 1]
+
+    for j in range(n):
+        result = sequence[-1] + sequence[-2]
+        sequence.append(result)
+
+    print(f'Fib({n}) = {sequence[n]}')
