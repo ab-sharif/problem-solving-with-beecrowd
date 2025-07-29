f55f490fa4590e57c474ec84d9966bce67b82d6f
Abu Bakkar Siddique Sharif <abubakkarsiddiquesharif@gmail.com>
2025-07-29 22:20:33 +0600

    solved(1190): Right Area

---
+++ 1190-Right Area/solution.py
@@ -0,0 +1,29 @@
+mode = input()
+n = 12
+matrix = []
+
+for r in range(n):
+    row = []
+    for items in range(n):
+        item = float(input())
+        row.append(item)
+
+    matrix.append(row)
+
+no_of_items = 0
+output = 0
+mid = int(n/2)
+for i in range(mid):
+
+    item_select_top = matrix[i][n-i:]
+    item_select_bottom = matrix[n-1-i][n-i:]
+
+    no_of_items += (len(item_select_top) + len(item_select_bottom))
+    output += (sum(item_select_bottom) + sum(item_select_top))
+
+if mode == 'S':
+    print(f'{output:.1f}')
+
+if mode == 'M':
+    output = output / no_of_items
+    print(f'{output:.1f}')
