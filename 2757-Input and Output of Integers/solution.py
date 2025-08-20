08a915ee03ed2be7aa63acada27b33c3a9b82f79
Abu Bakkar Siddique Sharif <abubakkarsiddiquesharif@gmail.com>
2025-08-20 23:50:25 +0600

    solved(2757): Input and Output of Integer

---
+++ 2757-Input and Output of Integers/solution.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(f'A = {a}, B = {b}, C = {c}')
+print(f'A = {a:>10}, B = {b:>10}, C = {c:>10}')
+print(f'A = {a:0=10}, B = {b:0>10}, C = {c:0>10}')
+print(f'A = {a:<10}, B = {b:<10}, C = {c:<10}')
