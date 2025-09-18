a1e9b68a555b2bb02f1ee18dac36a992b94bc12c
Abu Bakkar Siddique Sharif <abubakkarsiddiquesharif@gmail.com>
2025-09-19 00:17:42 +0600

    solved(10370: Interval

---
+++ 1037-Interval/solution.py
@@ -0,0 +1,24 @@
+val = float(input())
+
+lb = [0, 25, 50, 75]
+ub = [25, 50, 75, 100]
+lb_sign = '('
+ub_sign = ']'
+
+i = 0
+while i in range(4):
+    LB = lb[i]
+    UB = ub[i]
+    # print(i)
+
+    if val < 0 or val > 100:
+        print('Fora de intervalo')
+        break
+
+    elif LB <= val <= UB:
+        if val <= 25:
+            lb_sign = '['
+        print(f'Intervalo {lb_sign}{LB},{UB}{ub_sign}')
+        break
+
+    i += 1
